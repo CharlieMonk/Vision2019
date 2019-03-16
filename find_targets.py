7e95c07b8f5ebb6d21930716d932ffe934c5f016
Charlie Monk <charlie@charliemonk.net>
2019-03-16 16:37:59 -0400

    Fix multiple bugs to allow vision to run.

--- find_targets.py
+++ find_targets.py
@@ -24,65 +24,77 @@ def removeNoise(hsv_img, kernelSize, lower_color_range, upper_color_range):
     dilate = cv2.dilate(no_noise, np.ones((5,10), np.uint8), iterations=5)
     return dilate
 
-def findTargets(dilate, objName):
+def getCenterPoint(top_left, bottom_right):
+    return (int((top_left[0]+bottom_right[0])/2), int((top_left[1]+bottom_right[1])/2))
+
+def displayObject(contour_boundary, objName):
+    # If the object is cube, use red, if retroreflective, use blue
+    if(objName == "cube"):
+        color = (0,0,255)
+    elif(objName == "retroreflective"):
+        color = (255,0,0)
+
+    top_left, bottom_right = contour_boundary
+
+    # Draw a rectangle bounding the object using top left and bottom right points
+    cv2.rectangle(bgr_img, top_left, bottom_right, color, 3)
+    # Find the center point of the object
+    center_point = getCenterPoint(top_left, bottom_right)
+
+    # Draw circle at the center point
+    cv2.circle(bgr_img, center_point, 5, color, -1)
+
+    # isOffCenter = False # Enable if camera is off center
+    # angle = None
+    # if(isOffCenter): # If camera is NOT in center, use this
+    #     # Find the angle to the center point
+    #     offset = 90 # Change this as needed
+    #     adjusted_point = center_point[0] - offset
+    #     angle = getAngle(adjusted_point)
+    #     cv2.circle(bgr_img, (adjusted_point, center_point[1]), 5, color, -1)
+    # else:
+    #     # If camera IS in center, use this
+    #     angle = getAngle(center_point[0])
+    # print(objName, ":", angle)
+    # If the program isn't in testing mode, send data to RoboRIO
+    # if(sendPackets):
+    #     sendData(angle, width, objName)
+    return hsv_img
+
+def getContourBoundary(contour):
+    # Extract boundary points of object
+    left = tuple(contour[contour[:,:,0].argmin()][0])
+    right = tuple(contour[contour[:,:,0].argmax()][0])
+    top = tuple(contour[contour[:,:,1].argmin()][0])
+    bottom = tuple(contour[contour[:,:,1].argmax()][0])
+
+    # Use boundary points to find the top left and bottom right corners
+    top_left = (left[0], top[1])
+    bottom_right = (right[0], bottom[1])
+
+    return top_left, bottom_right
+
+def prepareForRoboRIO(contour_boundaries, objName):
+    contour_boundary_1, contour_boundary_2 = contour_boundaries
+
+    angle = getAngle(getCenterPoint(contour_boundary_1[0], contour_boundary_2[1])[0])
+    width = abs(contour_boundary_1[0][0] - contour_boundary_2[0][0]) # TODO : Check if subscripting is correct
+
+    sendData(angle, width, objName)
+
+def findObjectContours(dilate, objName):
     # Find boundary of object
-    _, contours, hierarchy = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # Only proceed if contours were found
     if(contours != None):
         if(len(contours) > 0):
-            # Find the largest contour
-            largest_area = 0
-            cnt = 0
-            for i in range(0, len(contours)):
-                area = cv2.contourArea(contours[i])
-                if(area > largest_area):
-                    largest_area = area
-                    cnt = contours[i]
-            # If the object is cube, use red, if retroreflective, use blue
-            if(objName == "cube"):
-                color = (0,0,255)
-            elif(objName == "retroreflective"):
-                color = (255,0,0)
-            # Extract boundary points of object
-            left = tuple(cnt[cnt[:,:,0].argmin()][0])
-            right = tuple(cnt[cnt[:,:,0].argmax()][0])
-            top = tuple(cnt[cnt[:,:,1].argmin()][0])
-            bottom = tuple(cnt[cnt[:,:,1].argmax()][0])
-
-            # Find and print the width of the cube
-            width = right[0] - left[0]
-            # print(objName + ": " + str(width))
-            # Use boundary points to find the top left and bottom right corners
-            top_left = (left[0], top[1])
-            bottom_right = (right[0], bottom[1])
-
-            # Draw a rectangle bounding the object using top left and bottom right points
-            cv2.rectangle(bgr_img, top_left, bottom_right, color, 3)
-            # Find the center point of the object
-            center_point = (int((top_left[0]+bottom_right[0])/2), int((top_left[1]+bottom_right[1])/2))
-
-            # Draw circle at the center point
-            cv2.circle(bgr_img, center_point, 5, color, -1)
-
-            isOffCenter = False # Enable if camera is off center
-            angle = None
-            if(isOffCenter): # If camera is NOT in center, use this
-                # Find the angle to the center point
-                offset = 90 # Change this as needed
-                adjusted_point = center_point[0] - offset
-                angle = getAngle(adjusted_point)
-                cv2.circle(bgr_img, (adjusted_point, center_point[1]), 5, color, -1)
-            else:
-                # If camera IS in center, use this
-                angle = getAngle(center_point[0])
-            print(objName, ":", angle)
-            # If the program isn't in testing mode, send data to RoboRIO
-            if(sendPackets):
-                sendData(angle, width, objName)
-            # Show the images
-            if(displayImages):
-                cv2.imshow("Mask Image", dilate)   # This should be enabled for debugging purposes ONLY!
-            return hsv_img
+            sorted(contours, key=lambda contour: cv2.contourArea(contour), reverse=True)
+            contour_boundaries = [getContourBoundary(contours[0]), getContourBoundary(contours[1])]
+            if sendPackets:
+                prepareForRoboRIO(contour_boundaries, objName)
+            for contour_boundary in contour_boundaries[:-1]:
+                displayObject(contour_boundary, objName)
+            return displayObject(contour_boundaries[-1], objName)
 
 def getAngle(point):
     # Use the center_point, fov, and width to find the heading (angle to target)
@@ -122,7 +134,7 @@ counter = 0
 # Track if the program has ran (if not, create a new folder for image logging)
 ranOnce = False
 # Folder path for logging images
-folder = "/var/log/Vision"
+folder = ""
 # Track if the program is being tested
 isTesting = False
 # Should the images be displayed on screen?
@@ -133,19 +145,21 @@ sendPackets = True
 shouldReduceExposure = True
 # If test is found in the cmd line arguments, then the program is testing
 for arg in sys.argv:
-    if(arg == "cube"):
-        shouldReduceExposure = False
-    if(arg == "test"):
-        # When testing, use an alternate filepath
-        folder = "/Users/cbmonk/Downloads/ImageLogging/"
-        isTesting = True
+    if(not isTesting):
+        if(arg == "cube"):
+            shouldReduceExposure = False
+        if(arg == "test"):
+            # When testing, use an alternate filepath
+            folder = "/Users/cbmonk/Downloads/ImageLogging/"
+            isTesting = True
+        else:
+            folder = "/var/log/Vision"
+            if(shouldReduceExposure):
+                reduceExposure()
     if(arg == "displayimages"):
         displayImages = True
     if(arg == "nopackets"):
         sendPackets = False
-
-if(shouldReduceExposure):
-    reduceExposure()
 
 # Setup UDP Channel
 rio_ip = "10.10.76.2"
@@ -198,7 +212,8 @@ if __name__ == "__main__":
     while(True):
         time0 = time.time()
         # Read the frame from the video capture
-        _, bgr_img = video_capture.read()
+        # _, bgr_img = video_capture.read()
+        bgr_img = cv2.imread("/Users/cbmonk/Downloads/test_image_2019_1.jpg")
         # Convert the frame to HSV
         hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
         # Enable line below if reading from precaptured image
@@ -208,7 +223,7 @@ if __name__ == "__main__":
         cube_hsv_lower = np.array([25, 100, 100])
         cube_hsv_upper = np.array([28, 255, 215])
         cube_dilate = removeNoise(hsv_img, (5,5), cube_hsv_lower,cube_hsv_upper)
-        cube_img = findTargets(cube_dilate, "cube")
+        cube_img = findObjectContours(cube_dilate, "cube")
 
         # Find the retroreflective tape
         # Use these HSV values if the LEDs are very bright and exposure is normal
@@ -217,10 +232,10 @@ if __name__ == "__main__":
         # Enable the below values if LEDs are NOT bright enough
         # retro_hsv_lower = np.array([87, 155, 230])
         # retro_hsv_upper = np.array([95, 200, 255])
-        retro_hsv_lower = np.array([43, 125, 171])
-        retro_hsv_upper = np.array([57, 255, 255])
+        retro_hsv_lower = np.array([0, 0, 0]) # np.array([43, 125, 171])
+        retro_hsv_upper = np.array([165, 23, 255])
         retro_dilate = removeNoise(hsv_img, (5,5), retro_hsv_lower, retro_hsv_upper)
-        retro_img = findTargets(retro_dilate, "retroreflective")
+        retro_img = findObjectContours(retro_dilate, "retroreflective")
 
         # Display the BGR image with found objects bounded by rectangles
         if(displayImages):
